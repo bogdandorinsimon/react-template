6ed298cb3e689c3472fc1089612e575e2adc02cd
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2023-08-08 11:08:24 +0300

    chore(setup): added vite types check

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true
+    })
+  ]
 });
