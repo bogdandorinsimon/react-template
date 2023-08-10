6a8b0e02a1df749aa97cccc3b5e0989eebaf23bd
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2023-08-10 21:06:20 +0300

    chore(setup): add support for jsdom (react testing library)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
+import type { InlineConfig } from "vitest";
 
-export default defineConfig({
+type ViteConfig = UserConfig & { test: InlineConfig };
+const config: ViteConfig = {
   plugins: [
     react(),
     tsconfigPaths(),
     checker({
       typescript: true
     })
-  ]
-});
+  ],
+  test: {
+    environment: "jsdom"
+  }
+};
+
+export default defineConfig(config);
